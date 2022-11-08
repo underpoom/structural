a4cf0ee8e881897e1043b6ddf66b163241d9f3da
underpoom <chaipanna.poom@gmail.com>
2022-11-08 22:54:03 +0700

    first commit

---
+++ src/HealthcareWorkerTeam.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable {
+    private Set<HealthcareServiceable> members = new HashSet<HealthcareServiceable>();
+    
+    public void addMember(HealthcareServiceable member){
+        members.add(member);
+    };
+
+    public void removeMember(HealthcareServiceable member){
+        members.remove(member);
+    };
+
+    public void service()
+    {
+        for(HealthcareServiceable member: members){
+            member.service();
+        }
+    };
+
+    public double getPrice() {
+        double price = 0;
+        for(HealthcareServiceable member: members){
+            price+=member.getPrice();
+        }
+        return price;
+    }
+}
